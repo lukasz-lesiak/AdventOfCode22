1c8daad23a1c30f31f64d9736070f86dbd68ff00
Lukasz Lesiak <lukasz.lesiak@idemia.com>
2022-12-04 08:20:06 +0100

    day4

---
+++ AdventOfCode22/puzzle8/Puzzle8.py
@@ -0,0 +1,17 @@
+# f = open("../inputs/day4_input.txt", "r")
+f = open("../inputs/day4_test_input.txt", "r")
+pairAssignments = f.read().split('\n')
+
+duplicatedRanges = 0
+for pairAssignment in pairAssignments:
+    assignments = pairAssignment.split(',')
+    e1 = assignments[0].split('-')
+    e2 = assignments[1].split('-')
+    elf1range = set(range(int(e1[0]), int(e1[1]) + 1))
+    elf2range = set(range(int(e2[0]), int(e2[1]) + 1))
+    print(f'E1: {elf1range} & E2: {elf2range}')
+    rangeIntersection = elf1range.intersection(elf2range)
+    if rangeIntersection.__len__() != 0:
+        duplicatedRanges += 1
+
+print(f'Result: {duplicatedRanges}')
